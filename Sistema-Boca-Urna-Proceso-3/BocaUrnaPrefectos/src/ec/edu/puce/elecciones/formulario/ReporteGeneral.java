f60bd97beb3d0408a48357d16da2565b105e9239
marloveper__ <154700999+MarLord06@users.noreply.github.com>
2024-07-16 23:32:53 -0500

    Add files via upload

---
+++ Sistema-Boca-Urna-Proceso-3/BocaUrnaPrefectos/src/ec/edu/puce/elecciones/formulario/ReporteGeneral.java
@@ -0,0 +1,69 @@
+package ec.edu.puce.elecciones.formulario;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import ec.edu.puce.elecciones.dominio.Prefecto;
+import java.awt.event.*;
+import java.util.List;
+
+public class ReporteGeneral extends JInternalFrame implements ActionListener {
+    private static final long serialVersionUID = 1L;
+
+    private JTable table;
+    private DefaultTableModel model;
+    private List<Prefecto> prefectos;
+    private JComboBox<String> comboBox;
+
+    public ReporteGeneral(List<Prefecto> prefectos) {
+        setClosable(true);
+        this.prefectos = prefectos;
+        setTitle("REPORTE GENERAL POR PROVINCIA");
+        setBounds(100, 100, 600, 309);
+        getContentPane().setLayout(null);
+
+        JScrollPane scrollPane = new JScrollPane();
+        scrollPane.setBounds(12, 48, 566, 167);
+        getContentPane().add(scrollPane);
+
+        table = new JTable();
+        table.setModel(new DefaultTableModel(new Object[][]{}, new String[]{"Nombre", "Partido", "Votos"}));
+        scrollPane.setViewportView(table);
+
+        JLabel lblProvincia = new JLabel("Provincia");
+        lblProvincia.setBounds(12, 21, 70, 15);
+        getContentPane().add(lblProvincia);
+
+        comboBox = new JComboBox<>(new String[]{
+                "Azuay", "Bolívar", "Cañar", "Carchi", "Chimborazo", "Cotopaxi",
+                "El Oro", "Esmeraldas", "Galápagos", "Guayas", "Imbabura", "Loja",
+                "Los Ríos", "Manabí", "Morona Santiago", "Napo", "Orellana",
+                "Pastaza", "Pichincha", "Santa Elena", "Santo Domingo de los Tsáchilas",
+                "Sucumbíos", "Tungurahua", "Zamora Chinchipe"
+        });
+
+        comboBox.setBounds(79, 12, 231, 24);
+        comboBox.addActionListener(this);
+        getContentPane().add(comboBox);
+
+        model = (DefaultTableModel) table.getModel();
+        llenarTabla();
+    }
+
+    private void llenarTabla() {
+        model.setRowCount(0);
+        String provinciaSeleccionada = (String) comboBox.getSelectedItem();
+        for (Prefecto prefecto : prefectos) {
+            int votosEnProvincia = prefecto.getVotosPorProvincia().getOrDefault(provinciaSeleccionada, 0);
+            if (votosEnProvincia > 0) {
+                model.addRow(new Object[]{prefecto.getNombre(), prefecto.getPartido(), votosEnProvincia});
+            }
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == comboBox) {
+            llenarTabla();
+        }
+    }
+}
